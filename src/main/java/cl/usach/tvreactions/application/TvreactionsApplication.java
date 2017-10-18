39f3ebac104e2382b9f42bfcf29c86ab41ec4145
ArielUndurraga <ariel.undurraga@usach.cl>
2017-10-18 11:24:43 -0700

    first commit

---
+++ src/main/java/cl/usach/tvreactions/application/TvreactionsApplication.java
@@ -0,0 +1,12 @@
+package cl.usach.tvreactions.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvreactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvreactionsApplication.class, args);
+	}
+}
---
+++ src/test/java/cl/usach/tvreactions/application/TvreactionsApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.usach.tvreactions.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TvreactionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
